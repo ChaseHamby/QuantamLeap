ac7fafbb55c364009a0348f8f5a8410a5ac36704
unknown <c.chase.hamby@gmail.com>
2019-05-04 11:50:35 -0500

    added leapee and leaper

---
+++ QuantamLeap/Controllers/LeapeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuantamLeap.Data;
+using QuantamLeap.Models;
+
+namespace QuantamLeap.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeapeeController : ControllerBase
+    {
+        readonly LeapeeRepository _leapeeRepository;
+
+        public LeapeeController()
+        {
+            _leapeeRepository = new LeapeeRepository();
+        }
+
+        [HttpPost("register")]
+
+        public ActionResult<int> AddLeaper(CreateLeapeeRequest createRequest)
+        {
+            //if (!_validator.Validate(createRequest))
+            //{
+            //    return BadRequest(new { error = "leapers must have a first name, last name, and target leap" });
+            //}
+
+            var newLeaper = _leapeeRepository.AddLeapee(createRequest.FirstName, createRequest.LastName, createRequest.Event);
+
+            return Created($"api/leapee/{newLeaper.Id}", newLeaper);
+        }
+    }
+}
---
+++ QuantamLeap/Data/LeapeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using QuantamLeap.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantamLeap.Data
+{
+    public class LeapeeRepository
+    {
+        const string ConnectionString = "Server = localhost; Database = QuantamLeap; Trusted_Connection = True;";
+
+        public Leapee AddLeapee(string firstName, string lastName, string Event)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newLeapee = db.QueryFirstOrDefault<Leapee>(@"
+                    Insert into Leapee (firstName,lastName,event) 
+                    Output inserted.*
+                    Values(@firstName,@lastName,@event)",
+                    new { firstName, lastName, Event });
+
+                if (newLeapee != null)
+                {
+                    return newLeapee;
+                }
+            }
+
+            throw new Exception("No leapee created");
+        }
+    }
+}
